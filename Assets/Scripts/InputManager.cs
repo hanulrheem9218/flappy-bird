d35d89e817fd975bc78b3e05aed27377d50129c1
hanulrheem9218 <114366902+hanulrheem9218@users.noreply.github.com>
2024-07-11 14:42:09 +1200

    Add a space button that makes player jump once.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    #region Unity Variables
+    [SerializeField, Header("Player's upper force ammount"),Range(0f,10f)]
+    private float upperForceAmount = 3.0f;
+    [SerializeField]
+    private KeyCode _jumpButton = KeyCode.Space;
+    [SerializeField]
+    private Rigidbody _playerRigidBody;
+    #endregion
     void Start()
     {
-        
+        //check if the rigidbody is being initialized by component.
+        if (!this.gameObject.TryGetComponent<Rigidbody>(out Rigidbody rigidBody))
+        {
+            this._playerRigidBody = gameObject.AddComponent<Rigidbody>();
+            print("Rigidbody has been added.");
+
+        }
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //check if the keyboard input has been pressed.
+        if (Input.GetKeyDown(_jumpButton))
+        {
+            _playerRigidBody.velocity = Vector3.up * upperForceAmount;  
+        }
     }
 }
